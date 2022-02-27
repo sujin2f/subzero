12497168168e0302ccaa083aa65a50518f68e6c9
Sujin Choi <sujin.2f@gmail.com>
2022-02-27 00:15:52 -0500

    #27 Refresh everyday

--- src/client/scenes/public.tsx
+++ src/client/scenes/public.tsx
@@ -2,7 +2,7 @@
  * Public Wrapper Component
  */
 
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment, useContext, useState, useEffect } from 'react'
 
 import { Context, ContextType } from 'src/client/store'
 import {
@@ -14,12 +14,21 @@ import {
 
 import 'src/assets/styles/style.scss'
 import { useUser } from '../hooks'
+import { getNextMidnight } from 'src/utils/datetime'
 
 export const Public = (): JSX.Element => {
     const user = useUser()
     const [{ removePopup }] = useContext(Context) as ContextType
     const classPublic = !user ? 'wrapper--public' : ''
     const classDarkMode = user?.darkMode ? 'wrapper--dark-mode' : ''
+    const [refreshTime, setRefreshTime] = useState(getNextMidnight())
+
+    useEffect(() => {
+        const toMidnight = refreshTime - new Date().getTime()
+        setTimeout(() => {
+            setRefreshTime(getNextMidnight())
+        }, toMidnight)
+    }, [refreshTime])
 
     return (
         <Fragment>
